c2143d39dc60b9e4f7b3e3cd81c93f900a323acf
BruteCoan <billyglenjr@gmail.com>
2026-08-17 19:05:39 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = "https://dffcmunbsoyajvudnfyl.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_e1yF6jOZNKaOIhsnxbcmKQ_7nYOOKiu";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+console.log("Culture Archive connected to Supabase");
